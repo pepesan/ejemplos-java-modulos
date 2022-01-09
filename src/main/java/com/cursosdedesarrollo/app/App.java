500e55ba820987f46a5754d69a7ac349624ccda3
David Vaquero <pepesan@gmail.com>
2022-01-09 20:46:57 +0100

    Módulos principal y submódulos en Maven

--- src/main/java/com/cursosdedesarrollo/app/App.java
+++ src/main/java/com/cursosdedesarrollo/app/App.java
@@ -1,4 +1,4 @@
-package com.cursosdedesarrollo;
+package com.cursosdedesarrollo.app;
 
 /**
  * Hello world!
--- submodulos/mainapp/src/test/java/com/cursosdedesarrollo/mainapp/AppTest.java
+++ submodulos/mainapp/src/test/java/com/cursosdedesarrollo/mainapp/AppTest.java
@@ -1,4 +1,4 @@
-package com.cursosdedesarrollo;
+package com.cursosdedesarrollo.mainapp;
 
 import static org.junit.Assert.assertTrue;
 
