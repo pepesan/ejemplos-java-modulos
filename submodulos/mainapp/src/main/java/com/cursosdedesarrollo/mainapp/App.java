ccfdef3ce7fee6bfa871997f3083cbfd4f0a3b7d
David Vaquero <pepesan@gmail.com>
2022-01-09 21:01:52 +0100

    usando la clase user desde mainapp

--- submodulos/mainapp/src/main/java/com/cursosdedesarrollo/mainapp/App.java
+++ submodulos/mainapp/src/main/java/com/cursosdedesarrollo/mainapp/App.java
@@ -1,5 +1,7 @@
 package com.cursosdedesarrollo.mainapp;
 
+import com.cursosdedesarrollo.hijo.User;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        User user = new User();
     }
 }
