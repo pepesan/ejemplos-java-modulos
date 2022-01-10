3508d98f2ca5c67f84109f57ec54c401d8a79c4d
David Vaquero <pepesan@gmail.com>
2022-01-10 12:45:09 +0100

    incluido lombok

--- submodulos/mainapp/src/main/java/com/cursosdedesarrollo/mainapp/App.java
+++ submodulos/mainapp/src/main/java/com/cursosdedesarrollo/mainapp/App.java
@@ -3,11 +3,7 @@ package com.cursosdedesarrollo.mainapp;
 
 import com.cursosdedesarrollo.hijo.User;
 
-/**
- * Hello world!
- *
- */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
