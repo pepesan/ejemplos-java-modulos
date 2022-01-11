8ab786b2ee523d196501f1998fa624af275bf839
David Vaquero <pepesan@gmail.com>
2022-01-11 15:59:03 +0100

    ejemplo de uso de streams reactivos en Java 9-11

---
+++ submodulos/reactivestreams/src/main/java/com/cursosdedesarrollo/reactivestreams/App.java
@@ -0,0 +1,26 @@
+package com.cursosdedesarrollo.reactivestreams;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Flow;
+import java.util.concurrent.SubmissionPublisher;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Ejemplos de Streams Reactivos!" );
+        ExecutorService executor = Executors.newFixedThreadPool(1);
+        SubmissionPublisher<String> sb = new SubmissionPublisher<>(executor, Flow.defaultBufferSize());
+        sb.subscribe(new MySubscriber());
+        sb.submit("item 1");
+        sb.submit("item 2");
+        sb.submit("item 3");
+
+        executor.shutdown();
+    }
+}
