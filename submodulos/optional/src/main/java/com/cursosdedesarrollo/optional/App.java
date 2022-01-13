e7b747407aa6c4923d2cc26aff4d3cf2c4f61a03
David Vaquero <pepesan@gmail.com>
2022-01-13 19:26:08 +0100

    mejoras del ejemplo de optional

--- submodulos/optional/src/main/java/com/cursosdedesarrollo/optional/App.java
+++ submodulos/optional/src/main/java/com/cursosdedesarrollo/optional/App.java
@@ -18,14 +18,17 @@ public class App
         IntStream.of(1, 2, 4)
                 .filter(i -> i % 3 == 0)
                 .findFirst()
-                .ifPresentOrElse(System.out::println, () -> {
-                    System.out.println("No multiple of 3 found");
-                });
+                .ifPresentOrElse(
+                        System.out::println, // Plan A
+                        () -> { // Plan B
+                            System.out.println("No multiple of 3 found");
+                        });
         // or
         char digit = Stream.of('a', 'b', 'c')
                 .filter(Character::isDigit)
                 .findFirst()
-                .or(() -> Optional.of('0')).get();
+                .or(() -> Optional.of('0')) // plan B
+                .get();
         System.out.println(digit);
         // Stream
         OptionalInt opt1 = IntStream.of(2, 5, 6).max();
