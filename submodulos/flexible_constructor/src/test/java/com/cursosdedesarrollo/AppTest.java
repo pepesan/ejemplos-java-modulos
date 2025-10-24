227715c39a2d685a9d1032f5b5221d20165e173d
David Vaquero <pepesan@gmail.com>
2025-10-24 12:23:18 +0200

    mejorados ejemplos de constructor flexible y de importación de módulos

--- submodulos/flexible_constructor/src/test/java/com/cursosdedesarrollo/AppTest.java
+++ submodulos/flexible_constructor/src/test/java/com/cursosdedesarrollo/AppTest.java
@@ -9,9 +9,16 @@ import org.junit.jupiter.api.Test;
 public class AppTest{
     @Test
     void testSumar() {
+        // Ejemplo 1: año anterior a 1886 → se corrige a 1886
         Coche coche = new Coche("  toyota  ", "Corolla", 1700);
         System.out.println("Marca final: " + coche.getMarca());   // TOYOTA
         System.out.println("Modelo final: " + coche.getModelo()); // Corolla
         System.out.println("Año final: " + coche.getAnio());      // 1886 (mínimo válido)
+
+        // Ejemplo 2: año válido
+        Coche coche2 = new Coche("Citroën", "C4", 2023);
+        System.out.println("Marca final: " + coche2.getMarca());
+        System.out.println("Modelo final: " + coche2.getModelo());
+        System.out.println("Año final: " + coche2.getAnio());
     }
 }
--- submodulos/module_import/src/test/java/com/cursosdedesarrollo/AppTest.java
+++ submodulos/module_import/src/test/java/com/cursosdedesarrollo/AppTest.java
@@ -2,7 +2,9 @@ package com.cursosdedesarrollo;
 
 
 import org.junit.jupiter.api.Test;
+// Usa List y Path sin imports de paquetes individuales
 import module java.base;
+import module java.xml;   // también podrías: java.sql, etc.
 
 public class AppTest {
     @Test
